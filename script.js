95b0b558fd9e3feef838ee53e1df1e40f75d43ef
Vinncent Miller <vinncentmiller@gmail.com>
2024-08-01 09:47:04 -0400

    Add primeTime

--- script.js
+++ script.js
@@ -37,3 +37,34 @@ for (let i = n - 1; i > 1; i--) {
 //     }
 
 // }
+
+function primeTime(n) {
+        // prime is a true/false value to be checked 
+        let prime;
+        // set n to next value after n to see if it is prime
+        n++;
+
+        // continues until prime is true
+        while (prime != true) {
+            // checks if prime
+            //temporarily sets prime to true
+            prime = true;
+            for (let i = n - 1; i > 1; i--) {
+                if (n % i == 0) {
+                    // if even 1 number is divisible, prime is false
+                    prime = false;
+                } 
+            }
+            if (prime == false) {
+                // if prime is still false, increases n by one
+                n++;
+            } else {
+                // if it makes it to here and prime is still true, new n is prime
+                console.log(`${n} is the next prime number.`)
+                prime = true;
+            }
+        }
+}
+
+// uncomment the next line placing any number there (sets n)
+// primeTime(8);
